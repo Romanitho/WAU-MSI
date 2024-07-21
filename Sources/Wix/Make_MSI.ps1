63e0845e46701544e9f4a88dfab4d98f7812bebe
Romain <96626929+Romanitho@users.noreply.github.com>
2024-07-21 10:00:06 +0200

    Update Make_MSI.ps1

--- Sources/Wix/Make_MSI.ps1
+++ Sources/Wix/Make_MSI.ps1
@@ -1,31 +1,31 @@
 [Cmdletbinding()]
 Param(
-	[Parameter(Mandatory = $false, Position = 0)]  [string] $Path = (Get-Location).Path,
-	[Parameter(Mandatory = $false)]  [string] $Sources = "$($Path)\Sources\WAU\Winget-AutoUpdate\",
-	[Parameter(Mandatory = $false)]  [string] $OutputFolder = $Path,
-	[Parameter(Mandatory = $false)]  [string] $IconFile = "$($Path)\Sources\Wix\files\icon.ico",
-	[Parameter(Mandatory = $false)]  [string] $BannerFile = "$($Path)\Sources\Wix\files\banner.bmp",
-	[Parameter(Mandatory = $false)]  [string] $DialogFile = "$($Path)\Sources\Wix\files\dialog.bmp",
-	[Parameter(Mandatory = $false)]  [string] $ProductId = "WAU",
-	[Parameter(Mandatory = $false)]  [string] $ProductName = "Winget-AutoUpdate",
-	[Parameter(Mandatory = $false)]  [string] $ProductVersion = "1.0.0",
-	[Parameter(Mandatory = $false)]  [string] $Manufacturer = "Romanitho",
-	[Parameter(Mandatory = $false)]  [string] $HelpLink = "https://github.com/Romanitho/Winget-AutoUpdate",
-	[Parameter(Mandatory = $false)]  [string] $AboutLink = "https://github.com/Romanitho/Winget-AutoUpdate",
-	[Parameter(Mandatory = $false)]  [string] $UpgradeCodeX86 = "B96866C0-EB44-4C0A-9477-2E5BB09CB9EF",
-	[Parameter(Mandatory = $false)]  [string] $UpgradeCodeX64 = "BDDEA607-F4AF-4229-8610-16E3B6455FDC",
-	[Parameter(Mandatory = $false)]  [switch] $PreRelease,
-	[Parameter(Mandatory = $false)]  [switch] $NoX86,
-	[Parameter(Mandatory = $false)]  [switch] $NoX64
+    [Parameter(Mandatory = $false, Position = 0)]  [string] $Path = (Get-Location).Path,
+    [Parameter(Mandatory = $false)]  [string] $Sources = "$($Path)\Sources\WAU\Winget-AutoUpdate\",
+    [Parameter(Mandatory = $false)]  [string] $OutputFolder = $Path,
+    [Parameter(Mandatory = $false)]  [string] $IconFile = "$($Path)\Sources\Wix\files\icon.ico",
+    [Parameter(Mandatory = $false)]  [string] $BannerFile = "$($Path)\Sources\Wix\files\banner.bmp",
+    [Parameter(Mandatory = $false)]  [string] $DialogFile = "$($Path)\Sources\Wix\files\dialog.bmp",
+    [Parameter(Mandatory = $false)]  [string] $ProductId = "WAU",
+    [Parameter(Mandatory = $false)]  [string] $ProductName = "Winget-AutoUpdate",
+    [Parameter(Mandatory = $false)]  [string] $ProductVersion = "1.0.0",
+    [Parameter(Mandatory = $false)]  [string] $Manufacturer = "Romanitho",
+    [Parameter(Mandatory = $false)]  [string] $HelpLink = "https://github.com/Romanitho/Winget-AutoUpdate",
+    [Parameter(Mandatory = $false)]  [string] $AboutLink = "https://github.com/Romanitho/Winget-AutoUpdate",
+    [Parameter(Mandatory = $false)]  [string] $UpgradeCodeX86 = "B96866C0-EB44-4C0A-9477-2E5BB09CB9EF",
+    [Parameter(Mandatory = $false)]  [string] $UpgradeCodeX64 = "BDDEA607-F4AF-4229-8610-16E3B6455FDC",
+    [Parameter(Mandatory = $false)]  [switch] $PreRelease,
+    [Parameter(Mandatory = $false)]  [switch] $NoX86,
+    [Parameter(Mandatory = $false)]  [switch] $NoX64
 )
 
 $ProgressPreference = "SilentlyContinue"
 
 # WiX paths
 If (!(Get-ChildItem -Path "$env:HOMEDRIVE\Program Files*\WiX*\" -Filter heat.exe -Recurse)) {
-	$ToolSetURL = "https://github.com/wixtoolset/wix3/releases/download/wix314rtm/wix314.exe"
-	Invoke-WebRequest -Uri $ToolSetURL -OutFile (Join-Path $Path "\wix.exe") -UseBasicParsing
-	Start-Process (Join-Path $Path "\wix.exe") -ArgumentList "/S" -Wait
+    $ToolSetURL = "https://github.com/wixtoolset/wix3/releases/download/wix314rtm/wix314.exe"
+    Invoke-WebRequest -Uri $ToolSetURL -OutFile (Join-Path $Path "\wix.exe") -UseBasicParsing
+    Start-Process (Join-Path $Path "\wix.exe") -ArgumentList "/S" -Wait
 }
 $wixDir = Split-Path ((((Get-ChildItem -Path "$env:HOMEDRIVE\Program Files*\WiX*\" -Filter heat.exe -Recurse) | Select-Object FullName)[0]).FullName)
 $heatExe = Join-Path $wixDir "heat.exe"
@@ -33,60 +33,60 @@ $candleExe = Join-Path $wixDir "candle.exe"
 $lightExe = Join-Path $wixDir "light.exe"
 
 if ($PreRelease) {
-	$Comment = "NIGHTLY"
-	$UpdatePreRelease = "#1"
+    $Comment = "NIGHTLY"
+    $UpdatePreRelease = "#1"
 }
 else {
-	$Comment = "STABLE"
-	$UpdatePreRelease = "#0"
+    $Comment = "STABLE"
+    $UpdatePreRelease = "#0"
 }
 
 # Platform settings
 $platforms = @()
 
 $x86Settings = @{
-	'arch'        = 'x86';
-	'sysFolder'   = 'System32';
-	'progfolder'  = 'ProgramFilesFolder';
-	'upgradeCode' = $UpgradeCodeX86;
-	'productName' = "${ProductName} (x86)";
-	'win64'       = 'no';
-	'outputMsi'   = (Join-Path $OutputFolder ($productID + "_x86.msi"))
+    'arch'        = 'x86';
+    'sysFolder'   = 'System32';
+    'progfolder'  = 'ProgramFilesFolder';
+    'upgradeCode' = $UpgradeCodeX86;
+    'productName' = "${ProductName} (x86)";
+    'win64'       = 'no';
+    'outputMsi'   = (Join-Path $OutputFolder ($productID + "_x86.msi"))
 }
 $x64Settings = @{
-	'arch'        = 'x64';
-	'sysFolder'   = 'Sysnative';
-	'progfolder'  = 'ProgramFiles64Folder';
-	'upgradeCode' = $UpgradeCodeX64;
-	'productName' = "${ProductName}";
-	'win64'       = 'yes';
-	'outputMsi'   = (Join-Path $OutputFolder ($productID + ".msi"))
+    'arch'        = 'x64';
+    'sysFolder'   = 'Sysnative';
+    'progfolder'  = 'ProgramFiles64Folder';
+    'upgradeCode' = $UpgradeCodeX64;
+    'productName' = "${ProductName}";
+    'win64'       = 'yes';
+    'outputMsi'   = (Join-Path $OutputFolder ($productID + ".msi"))
 }
 
 If (!$Nox86) {
-	$platforms += $x86Settings
+    $platforms += $x86Settings
 }
 If (!$Nox64) {
-	$platforms += $x64Settings
+    $platforms += $x64Settings
 }
 
 # Do the build
 foreach ($platform in $platforms) {
-	$platformArch = $platform.arch
-	$platformSysFolder = $platform.sysFolder
-	$platformProgFolder = $platform.progFolder
-	$platformUpgradeCode = $platform.upgradeCode
-	$platformProductName = $platform.productName
-	$platformWin64 = $platform.win64
+    $platformArch = $platform.arch
+    $platformSysFolder = $platform.sysFolder
+    $platformProgFolder = $platform.progFolder
+    $platformUpgradeCode = $platform.upgradeCode
+    $platformProductName = $platform.productName
+    $platformWin64 = $platform.win64
 
-	$modulesWxs = Join-Path $Path "_modules${platformArch}.wxs"
-	$productWxs = Join-Path $Path ".wxs${platformArch}"
-	$modulesWixobj = Join-Path $Path "_modules${platformArch}.wixobj"
-	$productWixobj = Join-Path $Path ".wixobj${platformArch}"
+    $modulesWxs = Join-Path $Path "_modules${platformArch}.wxs"
+    $productWxs = Join-Path $Path ".wxs${platformArch}"
+    $modulesWixobj = Join-Path $Path "_modules${platformArch}.wixobj"
+    $productWixobj = Join-Path $Path ".wixobj${platformArch}"
 
 
-	# Build XML
-	$wixXml = [xml] @"
+    # Build XML
+    $wixXml = [xml] @"
 <?xml version="1.0" encoding="utf-8"?>
 <Wix xmlns='http://schemas.microsoft.com/wix/2006/wi'>
 
@@ -392,20 +392,20 @@ foreach ($platform in $platforms) {
 "@
 
 
-	# Save XML and create productWxs
-	$wixXml.Save($modulesWxs)
-	& $heatExe dir $Sources -nologo -sfrag -sw5150 -ag -srd -gg -dir $ProductName -out $productWxs -cg INSTALLDIR -dr INSTALLDIR
+    # Save XML and create productWxs
+    $wixXml.Save($modulesWxs)
+    & $heatExe dir $Sources -nologo -sfrag -sw5150 -ag -srd -gg -dir $ProductName -out $productWxs -cg INSTALLDIR -dr INSTALLDIR
 
-	# Produce wixobj files
-	& $candleexe $modulesWxs -out $modulesWixobj
-	& $candleexe $productWxs -out $productWixobj
+    # Produce wixobj files
+    & $candleexe $modulesWxs -out $modulesWixobj
+    & $candleexe $productWxs -out $productWixobj
 }
 foreach ($platform in $platforms) {
-	$platformArch = $platform.arch
-	$modulesWixobj = Join-Path $Path "_modules${platformArch}.wixobj"
-	$productWixobj = Join-Path $Path ".wixobj${platformArch}"
-	$platformOutputMsi = $platform.outputMsi
+    $platformArch = $platform.arch
+    $modulesWixobj = Join-Path $Path "_modules${platformArch}.wixobj"
+    $productWixobj = Join-Path $Path ".wixobj${platformArch}"
+    $platformOutputMsi = $platform.outputMsi
 
-	# Produce the MSI file
-	& $lightexe -sval -sw1076 -spdb -ext WixUIExtension -ext WixUtilExtension -out $platformOutputMsi $modulesWixobj $productWixobj -b $Sources -sice:ICE91 -sice:ICE69 -sice:ICE38 -sice:ICE57 -sice:ICE64 -sice:ICE204 -sice:ICE80
+    # Produce the MSI file
+    & $lightexe -sval -sw1076 -spdb -ext WixUIExtension -ext WixUtilExtension -out $platformOutputMsi $modulesWixobj $productWixobj -b $Sources -sice:ICE91 -sice:ICE69 -sice:ICE38 -sice:ICE57 -sice:ICE64 -sice:ICE204 -sice:ICE80
 }
